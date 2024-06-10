c7232fffa996b4cfff4c1d9e98bc1b11e57a9a29
Louis <louis.dumont@viacesi.fr>
2024-06-10 09:52:00 +0200

    JENKINS TEST

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Order microservice')
+    .setTitle('Order microservice JENKINS TEST')
     .setVersion('1.0')
     .build();
 
